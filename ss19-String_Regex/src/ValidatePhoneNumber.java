45e9e28fa1bfe918f5e74ac1e0325676ac4c25bb
unknown <you@example.com>
2022-09-18 21:18:56 +0700

    add

--- ss19-String_Regex/src/ValidatePhoneNumber.java
+++ ss19-String_Regex/src/ValidatePhoneNumber.java
@@ -1,3 +1,19 @@
-public class ValidatePhoneNumber {
+import java.util.Scanner;
+import java.util.regex.Pattern;
 
+public class ValidatePhoneNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("Enter phone number: ");
+            String phone = sc.nextLine();
+            Pattern p = Pattern.compile("^\\([0-9]{2}\\)-\\(0{1}[0-9]{9}\\)$");
+            if (p.matcher(phone).find()) {
+                System.out.println("Valid.");
+                break;
+            } else {
+                System.err.println("Invalid");
+            }
+        }
+    }
 }
